ff6917a3bcfdc1981845e838ff2f52ae9f5f501d
j-tanz <john.tansor@cognizant.com>
2021-04-05 19:43:54 -0400

    adds test for posting post and expecting status created, sets up postDTO

---
+++ src/test/java/com/galvanize/guestbook/GuestbookTestIT.java
@@ -0,0 +1,36 @@
+package com.galvanize.guestbook;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.galvanize.guestbook.models.PostDTO;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class GuestbookTestIT {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @Test
+    public void addPost() throws Exception {
+        PostDTO postDTO = PostDTO.builder().name("Rajendra").build();
+
+        mockMvc.perform(post("/posts")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(postDTO)))
+                .andExpect(status().isCreated());
+    }
+}
