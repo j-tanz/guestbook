38bdbc15f2824dedf74ad1a05da72f9439e21911
j-tanz <john.tansor@cognizant.com>
2021-04-05 19:45:04 -0400

    refactors, removing unused imports

--- src/test/java/com/galvanize/guestbook/GuestbookTestIT.java
+++ src/test/java/com/galvanize/guestbook/GuestbookTestIT.java
@@ -1,13 +1,11 @@
 package com.galvanize.guestbook;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.galvanize.guestbook.models.PostDTO;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
