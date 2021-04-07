586e2178a30d365d3a1179a3738b7bf3c4f9e1f9
j-tanz <john.tansor@cognizant.com>
2021-04-07 19:58:03 -0400

    updates docker / heroku config

--- src/main/java/com/galvanize/guestbook/controller/PostController.java
+++ src/main/java/com/galvanize/guestbook/controller/PostController.java
@@ -3,9 +3,13 @@ package com.galvanize.guestbook.controller;
 import com.galvanize.guestbook.models.PostDTO;
 import com.galvanize.guestbook.service.GuestBookService;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -21,19 +25,12 @@ public class PostController {
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public void addPost(@RequestBody PostDTO post) {
-
         guestBookService.createPost(post);
-
-
     }
 
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
     public List<PostDTO> getAllPosts() {
-
         return guestBookService.getAllPosts();
-
-
     }
-
 }
