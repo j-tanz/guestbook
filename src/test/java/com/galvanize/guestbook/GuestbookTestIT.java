895eee7471306dea497b4d8ab8f15a2a83572910
Prasad Nettem <rajprasad.nettem@gmail.com>
2021-04-09 05:04:34 +0530

    green on adding rest document

--- src/test/java/com/galvanize/guestbook/GuestbookTestIT.java
+++ src/test/java/com/galvanize/guestbook/GuestbookTestIT.java
@@ -4,17 +4,22 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.galvanize.guestbook.models.PostDTO;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@AutoConfigureRestDocs
 public class GuestbookTestIT {
 
     @Autowired
@@ -25,15 +30,19 @@ public class GuestbookTestIT {
 
     @Test
     public void addPost() throws Exception {
-        PostDTO postDTO = PostDTO.builder().name("Rajendra").build();
+        PostDTO postDTO = PostDTO.builder().name("Rajendra").comment("Test comment").build();
 
         mockMvc.perform(post("/posts")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(postDTO)))
-                .andExpect(status().isCreated());
+                .andExpect(status().isCreated())
+                .andDo(document("addPost"));
         mockMvc.perform(get("/posts"))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("[0].name").value("Rajendra"));
+                .andExpect(jsonPath("[0].name").value("Rajendra"))
+                .andExpect(jsonPath("[0].comment").value("Test comment"))
+                 .andDo(document("Post",responseFields(fieldWithPath("[0].name").description("Author of the post"),
+                fieldWithPath("[0].comment").description("Post comment"))));
 
     }
 }
